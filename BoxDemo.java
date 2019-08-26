cef4c73116856dc87ab42e190093d088e24d36b4
chompa4 <54495407+chompa4@users.noreply.github.com>
2019-08-27 00:50:39 +0600

    27/8/19

---
+++ BoxDemo.java
@@ -0,0 +1,23 @@
+class Box {
+double width;
+double height;
+double depth;
+}
+class BoxDemo {
+	public static void main(String args[]) {
+	Box mybox1 = new Box();
+	Box mybox2 = new Box();
+	double vol;
+	mybox.width = 10;
+	mybox.height = 20;
+	mybox.depth = 15;
+	mybox2.width = 3;
+	mybox2.height = 6;
+	mybox2.depth = 9;
+	vol = mybox.width * mybox.height * mybox.depth;
+System.out.println("Volume is " + vol);
+
+	vol = mybox2.width * mybox2.height * mybox2.depth;
+System.out.println("Volume is " + vo1);
+}
+}
